78c6f48e1c6d67e6b3871c0c132c7ef60f77217a
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-01 16:20:00 +0000

    Bump version

--- src/Stripe.php
+++ src/Stripe.php
@@ -21,7 +21,7 @@ abstract class Stripe implements StripeInterface
      *
      * @var string
      */
-    const VERSION = '3.6.0';
+    const VERSION = '3.7.1';
 
     /* ------------------------------------------------------------------------------------------------
      |  Properties
