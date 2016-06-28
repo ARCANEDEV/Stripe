d21e160bd2058ea4e41e184f3ebb906a0b2e6776
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-28 03:38:12 +0000

    Cleaning the tests

--- tests/Utilities/UtilTest.php
+++ tests/Utilities/UtilTest.php
@@ -17,6 +17,40 @@ class UtilTest extends StripeTestCase
      | ------------------------------------------------------------------------------------------------
      */
     /** @test */
+    public function it_can_check_is_list()
+    {
+        $this->assertTrue(Util::isList([
+            5, 'nstaoush', []
+        ]));
+
+        $this->assertFalse(Util::isList([
+            5, 'nstaoush', [], 'bar' => 'baz'
+        ]));
+    }
+
+    /** @test */
+    public function can_check_that_php_has_value_semantics_for_arrays()
+    {
+        $original = $derived = ['php-arrays' => 'value-semantics'];
+        $derived['php-arrays'] = 'reference-semantics';
+
+        $this->assertSame('value-semantics', $original['php-arrays']);
+    }
+
+    /** @test */
+    public function it_convert_stripe_object_to_array_that_includes_id()
+    {
+        try {
+            $customer = self::createTestCustomer();
+
+            $this->assertArrayHasKey('id', $customer->toArray(true));
+        }
+        catch(\Exception $e) {
+            $this->markTestSkipped($e->getMessage());
+        }
+    }
+
+    /** @test */
     public function it_can_convert_stripe_object_to_array()
     {
         $array = Util::convertStripeObjectToArray([
